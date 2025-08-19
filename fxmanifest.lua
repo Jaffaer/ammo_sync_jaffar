845aded27931e82a00aea05c63a3caaeb294580c
Jaffar amin <bollibompad@gmail.com>
2025-08-19 15:38:05 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
+name 'ammo_sync_jaffar'
 author 'Jaffar'
 description 'Direct ammo sync with (ox_inventory and qbx-core)'
-version '1.0'
+version '1.0.0'
 
 client_script 'client.lua'
 server_script 'server.lua'
+
+dependency '/assetpacks'
