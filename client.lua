2eca453b13280300fda878e8238c14a167e1d1fc
Jaffar amin <bollibompad@gmail.com>
2025-08-19 15:38:40 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,3 +1,11 @@
+CreateThread(function()
+    local resourceName = GetCurrentResourceName()
+    if resourceName ~= "ammo_sync_jaffar" then
+        print("^1[ammo-sync] Fel resursnamn! Byt tillbaka till 'ammo_sync_jaffar'^0")
+        while true do Wait(1000) end 
+    end
+end)
+
 RegisterNetEvent('ammoSync:forceUpdate', function(weapon, ammo)
     local ped = PlayerPedId()
     if GetSelectedPedWeapon(ped) == weapon then
